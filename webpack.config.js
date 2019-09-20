67ac3adb2caa76a726854c30c539d08d055e6fd0
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2019-09-20 17:41:30 +0900

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  // mode: 'development',
+  entry: {
+    index: './index.ts',
+    'pdf-table-extractor': './pdf-table-extractor.ts'
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /.jsx?$/,
+      //   loader: 'babel-loader',
+      //   exclude: /node_modules/,
+      //   query: {
+      //     presets: ['@babel/preset-env'],
+      //   },
+      // },
+      {
+        test: /\.worker\.js$/,
+        use: { loader: 'worker-loader', options: { inline: true, fallback: false } }
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
