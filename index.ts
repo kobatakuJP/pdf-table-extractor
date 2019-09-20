bbba3a52574dcdff80edc7322eef16e8b647a506
kobatakuJP <36495393+kobatakuJP@users.noreply.github.com>
2019-09-20 18:09:18 +0900

    for demo index.ts

---
+++ index.ts
@@ -0,0 +1,3 @@
+import * as pte from "./pdf-table-extractor";
+(window as any).pdf_table_extractor = pte.pdf_table_extractor;
+(window as any).pdf_table_extractor_progress = pte.pdf_table_extractor_progress
